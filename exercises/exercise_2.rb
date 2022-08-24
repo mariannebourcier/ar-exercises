2f512f559448679dcb3367142e85ac0743e9d0de
mariannebourcier <marianne.bourcier@gmail.com>
2022-08-24 15:02:07 -0600

    add exercise 2 solution

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,13 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# load first store id=1 and assign instance var @store1
+# load second store and assign to @store2
+# update @store1 instance in db
+
+@store1 = Store.find_by(id: 1)
+puts "@store1 is #{@store1.name}"
+@store1.name = 'YYC'
+puts "@store1 is now #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+puts "@store2 is #{@store2.name}"
