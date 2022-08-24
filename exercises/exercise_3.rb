cd6c744692a4ad1efb9f517abb290e8c21581718
mariannebourcier <marianne.bourcier@gmail.com>
2022-08-24 15:08:19 -0600

    second exercise done

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# load store3
+# delete store
+# verify its deleted
+
+@store3 = Store.find_by(id: 3)
+puts "@store3 is #{@store3.name}"
+@store3.destroy
+puts "there are now #{Store.count} stores."
